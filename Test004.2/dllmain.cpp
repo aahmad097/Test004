f6ce48118bef2192ee1f86fba63bc756afefcc29
AliSAhmad <AliSAhmad@AAHMAD-X1CARBON>
2023-06-26 08:47:05 -0400

    Add project files.

---
+++ Test004.2/dllmain.cpp
@@ -0,0 +1,26 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+/// <summary>
+/// Computer\HKEY_CLASSES_ROOT\Directory\shellex\CopyHookHandlers
+/// </summary>
+/// <param name="hModule"></param>
+/// <param name="ul_reason_for_call"></param>
+/// <param name="lpReserved"></param>
+/// <returns></returns>
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    MessageBoxW(NULL, L"Via Copy Operation (CopyHookHandlers)", L"Persistence", MB_OK);
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
